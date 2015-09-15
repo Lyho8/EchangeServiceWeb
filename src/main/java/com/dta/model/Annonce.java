68bbed410bf6bf52999cab7786396fef5b275cc8
Lyho8 <aurelien.pluot@gmail.com>
2015-09-15 15:11:30 +0200

    Finition des classes de base hors mapping.

--- src/main/java/com/dta/model/Annonce.java
+++ src/main/java/com/dta/model/Annonce.java
@@ -2,6 +2,7 @@ package com.dta.model;
 
 import javax.persistence.*;
 import javax.validation.constraints.*;
+
 import java.util.*;
 
 @Entity
@@ -22,5 +23,59 @@ public class Annonce {
 	
 	@NotNull
 	private Date dateCreation;
+	
+	public Annonce(){
+		super();
+	}
+
+	public Annonce(int id, String description, boolean active, Type categorie,
+			Date dateCreation) {
+		super();
+		this.id = id;
+		this.description = description;
+		this.active = active;
+		this.categorie = categorie;
+		this.dateCreation = dateCreation;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public Type getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Type categorie) {
+		this.categorie = categorie;
+	}
+
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
 	
 }
