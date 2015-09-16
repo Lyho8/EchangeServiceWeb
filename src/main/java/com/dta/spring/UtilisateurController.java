39f31edefb0c0ab2a93022e542e2c34abb7305b0
fabien77200 <fabien.dutilleul37@gmail.com>
2015-09-16 14:57:19 +0200

    utilisateurController voire utilisateur.jsp

--- src/main/java/com/dta/spring/UtilisateurController.java
+++ src/main/java/com/dta/spring/UtilisateurController.java
@@ -6,29 +6,44 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.bankonet.model.Client;
 import com.dta.metier.IUtilisateurMetier;
 import com.dta.model.Utilisateur;
 
 @Controller
 public class UtilisateurController {
-	
-	@Resource(name="metier")
+
+	@Resource(name = "metier")
 	private IUtilisateurMetier ium;
 
-	//TODO ajouter un utilisateur.
+	// TODO ajouter un utilisateur.
 	@RequestMapping(value = "/utilisateur", method = RequestMethod.POST)
-	public String home(@Valid Utilisateur u, BindingResult bindingResult, Model model) {
-		
-		ium.creerUtilisateur(u);		
-		model.addAttribute("utilisateur", new Utilisateur());
-		
-		return "utilisateur";	
-	}
-	
+	public String home(@Valid Utilisateur u, BindingResult bindingResult,
+			Model model) {
 
-	
-	
+		if (u.getId() == 0)
+			ium.creerUtilisateur(u);
+		else
+			ium.actualiserUtilisateur(u);
+
+		model.addAttribute("utilisateur", new Utilisateur());
+
+		return "utilisateur";
+	}
+
+	// TODO actualiser un utilisateur.
+	@RequestMapping(value = "/editerUtilisateur/{id}")
+	public String updateClient(@PathVariable(value = "id") int id, Model model) {
+
+		Utilisateur u1 = ium.editerUtilisateur(id);
+
+		model.addAttribute("utilisateur", u1);
+
+		return "utilisateur";
+	}
+
 }
