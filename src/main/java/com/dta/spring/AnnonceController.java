2ede19bd1c0b10d218ff369e23b2d073caa2f9a3
minirop <minirop@hotmail.com>
2015-09-16 14:21:56 +0200

    ajout vues & controleur Annonce

---
+++ src/main/java/com/dta/spring/AnnonceController.java
@@ -0,0 +1,77 @@
+package com.dta.spring;
+
+import java.util.HashMap;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.dta.metier.IAnnonceMetier;
+import com.dta.model.Annonce;
+
+@Controller
+@RequestMapping(value = "annonces")
+public class AnnonceController {
+	@Resource(name="annonceMetier")
+	private IAnnonceMetier annonceMetier;
+	
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String home(Model model) {
+		return "annonces_home";
+	}
+	
+	@RequestMapping(value = "/nouvelle", method = RequestMethod.GET)
+	public String nouvelle(Model model) {
+		HashMap<String, String> types = new HashMap<String, String>();
+		types.put("Offre", "Offre");
+		types.put("Demande", "Demande");
+		
+		HashMap<Integer, String> categories = new HashMap<Integer, String>();
+		categories.put(1, "Plombier");
+		categories.put(2, "Electricien");
+		categories.put(3, "Mécanicien");
+		categories.put(4, "Jardinage");
+		
+		model.addAttribute("annonce", new Annonce());
+		model.addAttribute("types", types);
+		model.addAttribute("categories", categories);
+		
+		return "annonces_nouvelle";
+	}
+	
+	@RequestMapping(value = "/nouvelle", method = RequestMethod.POST)
+	public String creer(@Valid Annonce annonce, BindingResult result, Model model) {
+		
+		if(result.hasErrors()) {
+			HashMap<String, String> types = new HashMap<String, String>();
+			types.put("Offre", "Offre");
+			types.put("Demande", "Demande");
+			
+			HashMap<Integer, String> categories = new HashMap<Integer, String>();
+			categories.put(1, "Plombier");
+			categories.put(2, "Electricien");
+			categories.put(3, "Mécanicien");
+			categories.put(4, "Jardinage");
+			
+			model.addAttribute("annonce", annonce);
+			model.addAttribute("types", types);
+			model.addAttribute("categories", categories);
+			
+			return "annonces_nouvelle";
+		}
+		
+		annonceMetier.creerAnnonce(annonce);
+		return "redirect:/annonces/voir/" + annonce.getId();
+	}
+	
+	@RequestMapping(value = "/voir/{id}", method = RequestMethod.GET)
+	public String voir() {
+		return "annonces_annonce";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/annonces_home.jsp
@@ -0,0 +1,41 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib prefix="t" tagdir="/WEB-INF/tags"%>
+<t:template>
+	<jsp:body>
+<h1>
+	Liste des annonces
+</h1>
+
+<div class="row">
+  <div class="col-sm-2">
+    <div class="thumbnail">
+      <img src="http://www.mot-a-mot.com/media/annonce/annonce_2.jpg" alt="annonce de test">
+      <div class="caption">
+        <h3>Annonce n1</h3>
+        <p>Description de l'annonce, ceci concerne la dercription de l'annonce,
+         ceci concerne la dercription de l'annonce,
+          ceci concerne la dercription de l'annonce, 
+          ceci concerne la dercription de l'annonce
+        </p>
+        <p><a href="#" class="btn btn-primary" role="button">Voir l'annonce</a> </p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-2">
+    <div class="thumbnail">
+      <img src="http://www.mot-a-mot.com/media/annonce/annonce_2.jpg" alt="annonce de test">
+      <div class="caption">
+        <h3>Annonce n2</h3>
+        <p>Description de l'annonce, ceci concerne la dercription de l'annonce,
+         ceci concerne la dercription de l'annonce,
+          ceci concerne la dercription de l'annonce, 
+          ceci concerne la dercription de l'annonce
+        </p>
+        <p><a href="#" class="btn btn-primary" role="button">Voir l'annonce</a> </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </jsp:body>
+</t:template>
