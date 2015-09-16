f0d6b2076f34c072863dc241ab01de8371ec85b1
fabien77200 <fabien.dutilleul37@gmail.com>
2015-09-16 11:05:48 +0200

    user dao & metier

---
+++ src/main/java/com/dta/dao/IUserDao.java
@@ -0,0 +1,25 @@
+package com.dta.dao;
+
+import java.util.List;
+
+import com.dta.model.Utilisateur;
+
+
+public interface IUserDao {
+
+	public void creerUtilisateur(Utilisateur u) /*throws Exception*/; 
+
+	public List<Utilisateur> listerUtilisateurs();
+
+	public void supprimerUtilisateur(int idUtilisateur); 
+	
+	public Utilisateur editerUtilisateur(int idUtilisateur); 
+
+	public void actualiserUtilisateur(Utilisateur u);
+
+	public List<Utilisateur> chercherUtilisateurs(String motCle);
+
+	
+	
+	
+}
