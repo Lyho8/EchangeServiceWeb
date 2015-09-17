8b7adaa89a314d4c5af25251ea6e7bb925e49844
fabien77200 <fabien.dutilleul37@gmail.com>
2015-09-17 14:57:12 +0200

    liste et form bis

--- src/main/java/com/dta/controller/UtilisateurController.java
+++ src/main/java/com/dta/controller/UtilisateurController.java
@@ -1,5 +1,8 @@
 package com.dta.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.annotation.Resource;
 import javax.validation.Valid;
 
@@ -10,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.dta.metier.IMailService;
 import com.dta.metier.IUtilisateurService;
 import com.dta.model.Utilisateur;
 
@@ -20,23 +22,50 @@ public class UtilisateurController {
 	@Resource(name = "utilisateurMetier")
 	private IUtilisateurService ms;
 
-	@Resource(name = "mailMetier")
-	private IMailService m;
+	// @Resource(name = "mail")
+	// private IMailService m;
+	//
+
 	
+	// TODO afficher le formulaire
+	@RequestMapping(value = "/utilisateurs_liste", method = RequestMethod.GET)
+	public String listerUtilisateur(Model model) {
+
+		model.addAttribute("listeUtilisateurs", ms.listerUtilisateurs());
+
+		return "utilisateurs_liste";
+	}
+	
+	
+	// TODO afficher le formulaire
+	@RequestMapping(value = "/utilisateurBienvenue", method = RequestMethod.GET)
+	public String bienvenue(Model model) {
+
+		model.addAttribute("utilisateur", new Utilisateur());
+//		model.addAttribute("listeUtilisateurs", ms.listerUtilisateurs());
+
+		return "utilisateur";
+	}
+	
+
 	// TODO ajouter un utilisateur.
-	@RequestMapping(value = "/utilisateur", method = RequestMethod.POST)
+	@RequestMapping(value = "/utilisateurBdd", method = RequestMethod.POST)
 	public String ajout(@Valid Utilisateur u, BindingResult bindingResult,
 			Model model) {
 
-		if (u.getId() == 0){
-			System.out.println(m.email());
-			ms.creerUtilisateur(u);}
-		else
+		if (u.getId() == 0) {
+			// System.out.println(m.email());
+
+			Date time = new Date();
+			u.setDateInscription(time);
+			u.setSolde(10);
+			ms.creerUtilisateur(u);
+		} else
 			ms.actualiserUtilisateur(u);
 
 		model.addAttribute("utilisateur", new Utilisateur());
-		model.addAttribute("listeUtilisateurs", ms.listerUtilisateurs());
-		
+	//	model.addAttribute("listeUtilisateurs", ms.listerUtilisateurs());
+
 		return "utilisateur";
 	}
 
@@ -48,8 +77,14 @@ public class UtilisateurController {
 
 		model.addAttribute("utilisateur", u1);
 		model.addAttribute("listeUtilisateurs", ms.listerUtilisateurs());
-		
+
 		return "utilisateur";
+	}
+	
+	//TODO Login gestion de la connexion des utilisateurs.
+	@RequestMapping(value="/utilisateurLogin", method = RequestMethod.POST)
+	public String login(){
+		return null;
 	}
 
 }
