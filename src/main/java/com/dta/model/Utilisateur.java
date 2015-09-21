74e0621bc45d723b1207ab259e2d646c9fba3c2c
minirop <minirop@hotmail.com>
2015-09-21 09:56:43 +0200

    fix login

--- src/main/java/com/dta/model/Utilisateur.java
+++ src/main/java/com/dta/model/Utilisateur.java
@@ -6,13 +6,14 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -28,28 +29,28 @@ public class Utilisateur {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
-	@NotNull
+	//@NotNull
 	@Size(max = 60, min = 3)
 	private String nom = "";
 
-	@Size(max = 60, min = 3)
+	//@Size(max = 60, min = 3)
 	private String prenom = "";
 
-	@Size(max = 255, min = 6)
+	//@Size(max = 255, min = 6)
 	private String email = "";
 
-	@NotNull
-	@Size(max = 16, min = 2)
-	@Column(unique = true)
+	//@NotNull
+	//@Size(max = 16, min = 2)
+	//@Column(unique = true)
 	private String login;
 
-	@NotNull
-	@Size(max = 50, min = 6)
+	//@NotNull
+	//@Size(max = 50, min = 6)
 	private String motDePasse;
 
 	private boolean actif;
 
-	@NotNull
+	//@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date dateInscription;
 
@@ -75,6 +76,7 @@ public class Utilisateur {
 	@OneToMany(mappedBy = "recepteur")
 	private List<Paiement> paiementsRecus;
 
+	@Enumerated(EnumType.STRING)
 	private Role role = Role.ROLE_USER;
 
 	public Utilisateur() {
